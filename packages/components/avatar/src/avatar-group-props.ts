b5568f2c40d1de09d5d3af6a6dccb83e85ee3ba9
IceyWu <3128006406@qq.com>
2024-04-26 16:31:52 +0800

    feat: add popover

--- packages/components/avatar/src/avatar-group-props.ts
+++ packages/components/avatar/src/avatar-group-props.ts
@@ -1,16 +1,38 @@
+import { useTooltipTriggerProps } from '@element-plus/components/tooltip'
 import { avatarProps } from './avatar'
 
 import type { ExtractPropTypes } from 'vue'
 
 export const avatarGroupProps = {
-  options: {
-    type: Array,
-    default: () => [],
-  },
+  /**
+   * @description maximum number of avatars.
+   */
   max: {
     type: Number,
     default: 4,
   },
-  // type: avatarProps.type,
+  /**
+   * @description avatar shape.
+   */
+  shape: avatarProps.shape,
+  /**
+   * @description the source of the image for an image avatar.
+   */
+  size: avatarProps.size,
+  /**
+   * @description How should the tooltip be triggered (to show)
+   */
+  maxPopoverTrigger: useTooltipTriggerProps.trigger,
+  /**
+   * @description the placement of the tooltip
+   */
+  maxPopoverPlacement: useTooltipTriggerProps.placement,
+  /**
+   * @description the placement of the tooltip
+   */
+  maxStyle: {
+    type: Object,
+    default: () => ({}),
+  },
 } as const
 export type AvatarGroupProps = ExtractPropTypes<typeof avatarGroupProps>
--- packages/components/avatar/src/avatar-group.tsx
+++ packages/components/avatar/src/avatar-group.tsx
@@ -1,5 +1,6 @@
 import { computed, defineComponent, nextTick, ref, unref } from 'vue'
 import { useNamespace } from '@element-plus/hooks'
+import { ElPopover } from '@element-plus/components/popover'
 import { avatarGroupProps } from './avatar-group-props'
 import Avatar from './avatar.vue'
 import type { AvatarGroupProps } from './avatar-group-props'
@@ -10,20 +11,29 @@ const AvatarGroup = defineComponent({
   name: COMPONENT_NAME,
   props: avatarGroupProps,
   setup(props: AvatarGroupProps, { slots, expose }) {
-    // console.log('🦄-----props-----', props)
+    console.log('🦄-----props-----', props)
     // console.log('🌳-----slots-----', slots)
     const ns = useNamespace('avatar')
     const children = computed(() => slots.default?.()?.[0].children) as any
 
     const showRenderSlot = () => {
-      const { max } = props
+      const { max, shape, size, maxPopoverTrigger, maxPopoverPlacement } = props
       if (children.value.length > max) {
         const content = children.value || []
         const outAvatar = content.slice(0, max) as any
+        const allList = slots.default?.()
         const maxItem = slots.maxPart ? (
+          // <ElPopover trigger={maxPopoverTrigger}>{slots.maxPart()}</ElPopover>
           slots.maxPart()
         ) : (
-          <Avatar>+{max}</Avatar>
+          <ElPopover
+            trigger={maxPopoverTrigger}
+            placement={maxPopoverPlacement}
+            v-slots={{
+              reference: () => <Avatar>+{max}</Avatar>,
+              default: () => allList,
+            }}
+          ></ElPopover>
         )
         outAvatar.push(maxItem)
         console.log('🌳-----outAvatar-----', outAvatar)
